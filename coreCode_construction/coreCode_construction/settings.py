d4970b1cc69fef5a77b2825daebeb740ff100fde
meinayatraza <me.inayatraza@gmail.com>
2026-05-24 10:18:17 +0500

    setting up base.html

--- coreCode_construction/coreCode_construction/settings.py
+++ coreCode_construction/coreCode_construction/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'project_core',
+    'material_estimator',
+    'user_accounts',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +61,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
--- coreCode_construction/coreCode_construction/urls.py
+++ coreCode_construction/coreCode_construction/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.core.urls')),
+    path('', include('project_core.urls')),
 ]
 
 # Serve media files in development
